85b2cb4ef7ce6afed6dec94c84de58bee46499b3
Himanshu Gupta <89313623+repo-tech@users.noreply.github.com>
2026-04-20 14:19:46 +0530

    Time: 0 ms (100%), Space: 60.4 MB (54.16%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    if(!nums || nums.length === 0) {
+        return;
+    }
+    let left = 0;
+    for(let right = 0; right < nums.length; right++) {
+        if(nums[right] !== 0) {
+            nums[left] = nums[right];
+            left++;
+        }
+    }
+    for(let i = left; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+
+};
