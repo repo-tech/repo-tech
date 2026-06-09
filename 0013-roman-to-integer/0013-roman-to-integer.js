e5ea6ab6f6061d1b173fd557f9534f47f1c41706
Himanshu Gupta <89313623+repo-tech@users.noreply.github.com>
2026-06-09 15:21:08 +0530

    Time: 3 ms (91.59%), Space: 60.9 MB (65.8%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+    
+    let total = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        let currentVal = romanMap[s[i]];
+        let nextVal = romanMap[s[i + 1]];
+        
+        if (nextVal > currentVal) {
+            total -= currentVal;
+        } else {
+            total += currentVal;
+        }
+    }
+    
+    return total;
+};
