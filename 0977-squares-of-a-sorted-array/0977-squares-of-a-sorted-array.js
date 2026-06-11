536343b1b504ed916d19fe1626589cb0f778705b
Himanshu Gupta <89313623+repo-tech@users.noreply.github.com>
2026-06-11 10:16:44 +0530

    Time: 1 ms (98.95%), Space: 60.6 MB (95.9%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -2,15 +2,30 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
 var sortedSquares = function(nums) {
-    let left = 0, right = nums.length-1;
-    let arr = [];
+    let left = 0;
+    let right = nums.length - 1;
+    
+    let arr = new Array(nums.length); 
+    let p = nums.length - 1; 
 
-    while(left<=right){
-        let rs = Math.abs(nums[left])*Math.abs(nums[left]);
-        arr.push(rs);
-        left+=1;
+    while (left <= right) {
+        let leftSquare = nums[left] * nums[left];
+        let rightSquare = nums[right] * nums[right];
+
+        if (leftSquare > rightSquare) {
+            arr[p] = leftSquare;
+            left++; 
+        } else {
+            arr[p] = rightSquare;
+            right--; 
+        }
+        p--; 
     }
 
-    return arr.sort((a,b)=>(a-b))
+    return arr;
 };
